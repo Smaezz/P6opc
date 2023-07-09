9537a8d9a20f82bd150c5e6dcd83b6c5bbb1179a
Smaezz <lea.msi@hotmail.fr>
2023-07-09 17:50:48 +0200

    Login ok

--- FrontEnd/assets/scriptConnexion.js
+++ FrontEnd/assets/scriptConnexion.js
@@ -35,37 +35,46 @@ console.log(form);
 */
 async function login() {
 const form = document.querySelector('form');
-const error = document.getElementById('error');
+//const error = document.getElementById('error');
 
 // Quand on submit
-form.addEventListener('submit', (event) => { 
+form.addEventListener('submit', async(event) => { 
 // On empêche le comportement par défaut
-  //event.preventDefault();
+event.preventDefault();
 // Récupération des valeurs du formulaire 
-let email = document.getElementById("email").value;
-let motPasse = document.getElementById("password").value;
-// Création de l'objet de données à envoyer à l'API
-let dataP = [email, motPasse]
-console.log(dataP);
+//let email = document.getElementById("email").value;
+//let motPasse = document.getElementById("password").value;
+ // on récupère les données du formulaire dans une constante
+ let formData = {
+  email: event.target.querySelector("[name=email]").value,
+  password: event.target.querySelector("[name=password]").value,
+}
+// convertir l'email et le password en objet JSON 
+//const dataP = JSON.stringify(formData)
+//Création de l'objet de données à envoyer à l'API
+//let dataP = {email,
+ //            motPasse}
+
 // Envoi de la requête à l'API 
 fetch('http://localhost:5678/api/users/login', {
     method: 'POST',
     headers: {    
-'Content-Type': 'application/json'
+       'Content-Type': 'application/json'
     },
-    body: JSON.stringify(dataP)
+    body: JSON.stringify(formData)
     })
    
     .then(response => response.json())
+    //console.log(response)
     .then(data => {
       if (data.token) {
-        localStorage.setItem("token".data.token)
+        localStorage.setItem("token",data.token)
         window.location.href="index.html"
       }
       
       else { 
 // La requête a échoué
-        error.textContent="Erreur lors de la requête"
+        error.textContent="Email ou mot de passe incorrect"
       }
     })
   })
